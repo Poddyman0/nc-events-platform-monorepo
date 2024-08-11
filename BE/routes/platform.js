e79ae79cb3e7b9b3befb65d3710cbaeaa6c1014e
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-08-11 10:26:56 +0100

    FE in public

--- BE/routes/platform.js
+++ BE/routes/platform.js
@@ -95,9 +95,6 @@ router.post("/event/delete/:eventID/invitee/:inviteeID", jwtAuth,
 event_controller.deleteAInviteeForAEvent
 )
 
-app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'FE', 'index.html'));
-  });  
 
 
 module.exports = router;
